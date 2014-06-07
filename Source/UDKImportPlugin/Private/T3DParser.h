3db6e6a8a91625a6491d90b1e8357f0f712f93fc
Vincent (Speedy37) Rouille <vincent@speedy37.fr>
2014-06-07 14:28:21 +0200

    Header comments and reorganisation

--- Source/UDKImportPlugin/Private/T3DParser.h
+++ Source/UDKImportPlugin/Private/T3DParser.h
@@ -10,38 +10,47 @@ private:
 	static float UnrRotToDeg;
 	static float IntensityMultiplier;
 
-	TArray<FString> Lines;
-	FString Line;
-
-	int32 LineIndex, ParserLevel;
+	/// Ressources requirements
 	TMap<FString, FExecuteAction> Requirements;
 	UObject * CurrentRequirement;
-	UWorld * World;
+	void AddRequirement(const FString &UDKRequiredObjectName, FExecuteAction Action);
 
+	/// Line parsing
+	int32 LineIndex, ParserLevel;
+	TArray<FString> Lines;
+	FString Line;
 	bool NextLine();
 	bool IgnoreSubs();
 	bool IgnoreSubObjects();
 	void JumpToEnd();
+
+	/// Line content parsing
 	bool IsBeginObject(FString &Class, FString &Name);
 	bool IsEndObject();
-	bool GetOneValueAfter(const FString &Key, FString &Value);
-	bool GetProperty(const FString &Key, FString &Value);
-	void AddRequirement(const FString &UDKRequiredObjectName, FExecuteAction Action);
-	bool ParseUDKRotation(const FString &InSourceString, FRotator &Rotator);
-	bool ParseFVector(const TCHAR* Stream, FVector& Value);
 	bool IsActorLocation(AActor * Actor);
 	bool IsActorRotation(AActor * Actor);
 	bool IsActorScale(AActor * Actor);
 
+	/// Value parsing
+	bool GetOneValueAfter(const FString &Key, FString &Value);
+	bool GetProperty(const FString &Key, FString &Value);
+	bool ParseUDKRotation(const FString &InSourceString, FRotator &Rotator);
+	bool ParseFVector(const TCHAR* Stream, FVector& Value);
+
+	/// Actor creation
+	UWorld * World;
 	template<class T>
 	T * SpawnActor();
 
+	/// Actor Importation
 	void ImportBrush();
 	void ImportPolyList(UPolys * Polys);
 	void ImportStaticMeshActor();
 	void ImportPointLight();
 	void ImportSpotLight();
 	USoundCue * ImportSoundCue();
+
+	/// Available ressource actions
 	void SetStaticMesh(UStaticMeshComponent * StaticMeshComponent);
 	void SetPolygonTexture(UPolys * Polys, int32 index);
 	void SetSoundCueFirstNode(USoundCue * SoundCue);
